a5818249631ca85da3996b126e4f4ca8bb20a16b
Adam Radford (aradford) <aradford@cisco.com>
2022-10-27 13:30:22 +1100

    alternative way given API limitation in 2.2.2.x

--- mapsiteSSID.py
+++ mapsiteSSID.py
@@ -24,8 +24,7 @@ def format_time(secs):
     return timestr
 
 def print_site(sitestr, profile):
-    #print(profile)
-    ssid = ",".join([ssid.name for ssid in profile.profileDetails.ssidDetails if 'name' in ssid])
+    ssid = ",".join([ssid['name'] for ssid in profile.profileDetails.ssidDetails if 'name' in ssid])
     name = profile.profileDetails.name
     for site in profile.profileDetails.sites:
         if sitestr is None or sitestr in site:
@@ -37,8 +36,56 @@ def match_sites(sitestr, sitelist):
     matches = [site  for site in sitelist if sitestr in site]
     return True if matches != [] else False
 
+def build_profile_cache(dnac):
+    # this is for version 2.2.2.x where the get for wireless profile is broken.  Can only do a get for a named profile
+    profiles = dnac.custom_caller.call_api(method="GET", resource_path="api/v1/siteprofile/attribute?key=wireless.ssid")
+    #result = [profile['name'] for profile in profiles['response']]
+    #print(profiles)
+    result = [profile['siteProfileUuid'] for profile in profiles['response']]
+    return result
+
+class ProfileDetails:
+    def __init__(self,name, sites, ssid):
+        self.name = name
+        self.sites = sites
+        self.ssidDetails = ssid
+class Profile:
+    def __init__(self, details):
+        self.profileDetails = details
+
+def map_site_id_to_name(dnac, siteid):
+    response = dnac.sites.get_site(siteId=siteid)
+    return response.response.siteNameHierarchy
+
+def get_internal_profile(dnac,profileid):
+    # using this as the wireless_profile API is rate limited to 5/min
+    profile = dnac.custom_caller.call_api(method="GET", resource_path="api/v1/siteprofile/{}?includeSites=true&excludeSettings=true&populated=true".format(profileid))
+    p = profile.response
+    ssid = [ {'name': s['value']} for s in p.profileAttributes if s['value'] is not None ]
+    
+    #sites = [site['name'] for site in p.sites]
+    # convert site id to name to get Fully qualified site name
+    sites = [map_site_id_to_name(dnac, site['uuid']) for site in p.sites]
+    p = Profile(ProfileDetails(p.name, sites, ssid))
+    return p 
+
+def get_profiles(dnac):
+    profileids = build_profile_cache(dnac)
+    profiles=[]
+    for profileid in profileids:
+        #profile = dnac.wireless.get_wireless_profile(profileName=profilename)
+        profile = get_internal_profile(dnac,profileid)
+    
+        profiles.append(profile)
+    return profiles
+        
 def main(dnac,sitestr):
-    profiles = dnac.wireless.get_wireless_profile()
+    try:
+         #raise ValueError("as")
+         profiles = dnac.wireless.get_wireless_profile()
+#    except ApiError:
+    except ValueError:
+         profiles = get_profiles(dnac)
     print(FMTSTR.format("site","profilename","ssid"))
     for profile in profiles:
         if sitestr or match_sites(sitestr, profile.profileDetails.sites):
