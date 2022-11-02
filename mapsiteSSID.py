6a12a14ba304727b7b2f6245c7d3bdca0bdcf3c5
Adam Radford (aradford) <aradford@cisco.com>
2022-11-02 17:38:34 +1100

    add a site cache for legacy profile collection to get around API limit

--- mapsiteSSID.py
+++ mapsiteSSID.py
@@ -53,11 +53,41 @@ class Profile:
     def __init__(self, details):
         self.profileDetails = details
 
+
+# dont use this as it breaks with
 def map_site_id_to_name(dnac, siteid):
     response = dnac.sites.get_site(siteId=siteid)
     return response.response.siteNameHierarchy
 
-def get_internal_profile(dnac,profileid):
+LIMIT = 500
+#LIMIT = 10
+class SiteCache:
+    def __init__(self, dnac):
+        self._cache = {}
+        self.dnac = dnac
+        response = dnac.sites.get_site_count()
+        count = response.response
+        logger.debug ("COUNT:{}".format(count))
+
+        for start in range(1, count+1, LIMIT):
+            #response = get_url("group?groupType=SITE&offset={}&limit={}".format(start,LIMIT))
+            response = dnac.sites.get_site(offset=str(start),limit=str(LIMIT))
+
+            sites = response['response']
+            for s in sites:
+                logger.debug("Caching {}".format(s.siteNameHierarchy))
+                self._cache[s.id] = s.siteNameHierarchy
+
+        logger.debug("cache size is {}".format(len(self._cache)))
+    def lookup_by_id(self, siteid):
+        logger.debug("looking up {}".format(siteid))
+        if siteid in self._cache:
+            return self._cache[siteid]
+        else:
+            raise ValueError("Cannot find site:{}".format(siteid))
+
+
+def get_internal_profile(dnac,sitecache,profileid):
     # using this as the wireless_profile API is rate limited to 5/min
     profile = dnac.custom_caller.call_api(method="GET", resource_path="api/v1/siteprofile/{}?includeSites=true&excludeSettings=true&populated=true".format(profileid))
     p = profile.response
@@ -66,33 +96,55 @@ def get_internal_profile(dnac,profileid):
     #sites = [site['name'] for site in p.sites]
     # convert site id to name to get Fully qualified site name
     if p.sites:
-        sites = [map_site_id_to_name(dnac, site['uuid']) for site in p.sites]
+        #sites = [map_site_id_to_name(dnac, site['uuid']) for site in p.sites]
+        sites = [sitecache.lookup_by_id(site['uuid']) for site in p.sites]
     else:
         sites = []
     p = Profile(ProfileDetails(p.name, sites, ssid))
     return p 
 
 def get_profiles(dnac):
+    sitecache = SiteCache(dnac)
     profileids = build_profile_cache(dnac)
     profiles=[]
     for profileid in profileids:
         #profile = dnac.wireless.get_wireless_profile(profileName=profilename)
-        profile = get_internal_profile(dnac,profileid)
+        profile = get_internal_profile(dnac, sitecache, profileid)
     
         profiles.append(profile)
     return profiles
         
-def main(dnac,sitestr):
+def collect_sites(dnac):
     try:
 #         raise ValueError("as")
          profiles = dnac.wireless.get_wireless_profile()
     except ApiError:
 #    except ValueError:
          profiles = get_profiles(dnac)
+    return profiles
+
+def print_profiles(profiles, sitestr):
     print(FMTSTR.format("site","profilename","ssid"))
     for profile in profiles:
         if sitestr or match_sites(sitestr, profile.profileDetails.sites):
              print_site(sitestr, profile)
+
+def get_wlc_id(dnac, ip="10.10.10.146"):
+    try:
+        response = dnac.devices.get_network_device_by_ip(ip_address=ip)
+    except ApiError as e:
+        print("Device {} not found".format(ip))
+        sys.exit(1)
+    return response.id
+
+def get_wlc_site_list(dnac, id):
+    pass
+
+def main(dnac, sitestr):
+    profiles = collect_sites(dnac)
+    print_profiles(profiles, sitestr)
+#    wlcid = get_wlc_id(dnac)
+
 
 if __name__ == "__main__":
     parser = ArgumentParser(description='Select options.')
@@ -112,7 +164,8 @@ if __name__ == "__main__":
     #logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
     dnac = api.DNACenterAPI(base_url='https://{}:443'.format(DNAC),
-                                username=DNAC_USER,password=DNAC_PASSWORD,verify=False,debug=True)
+                                #username=DNAC_USER,password=DNAC_PASSWORD,verify=False,debug=True)
+                                username=DNAC_USER,password=DNAC_PASSWORD,verify=False)
 
     main(dnac, args.sitestr)
 
